5345bbba05350a646fb68655ac759a5145ccf89b
Mikhaela Cueto <mikhaelacueto@kinpo.com.ph>
2017-08-10 17:33:01 +0800

    spotify website

---
+++ app/controllers/SpotifyTop.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+/**
+ * Created by PB5N0029 on 8/10/2017.
+ */
+public class SpotifyTop extends Controller{
+    public static void index(){
+        render();
+    }
+}
+
